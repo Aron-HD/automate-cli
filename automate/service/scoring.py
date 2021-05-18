ac1d232b2fdee80f9d2495f2852e7573f6791450
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-05-18 01:49:16 +0100

    worked on CreateAsset class

--- automate/service/scoring.py
+++ automate/service/scoring.py
@@ -64,16 +64,108 @@ class Scoresheet:
         else:
             print(f'Incorrect filename info from {self.file.name}', info)
             judge_info = None
+
+        if judge_info:
+            try:
+                # Extract group integer from group string
+                judge_info['Group'] = int(
+                    ''.join(filter(str.isdigit, judge_info['Group'])))
+            except TypeError as e:
+                raise e
         return judge_info
+
+
+class CreateAsset(object):
+    """docstring for CreateAsset"""
+    JUDGES_COLS = ['Name', 'Surname', 'Company', 'Group']
+    PAPER_COLS = ['PaperGroup', 'ID', 'Ref', 'Title']
+
+    def __init__(self, path, outfile, award,
+                 create_type, excel_file):
+        self.path = path
+        self.outfile = outfile
+        self.award = award
+        self.create_type = create_type
+        self.excel_file = excel_file
+
+    @staticmethod
+    def get_groups(frame: pd.DataFrame, cols: list):
+        """
+        Split papers or judges into separate groups returned as an enumerated list of dataframes.
+        """
+        dfg = frame[cols].dropna(axis=0)
+        gc = list(filter(lambda col: 'Group' in col, cols))
+        dfg[gc[0]] = dfg[gc[0]].astype(int)
+        # get unique group values
+        groups = list(dfg[gc[0]].unique())
+        print(groups)
+        # filter papers by group, n+1 as mismatch of group start at 1 vs index 0
+        grouped_frames = [
+            dfg[dfg[gc[0]] == n] for n in range(1, len(groups)+1)
+        ]
+        return enumerate(grouped_frames, start=1)
+
+    def consolidated_marks(self, excel_sheet):
+        """Make consolidated marks spreadsheet from main spreadsheet data."""
+        frame1 = pd.read_excel(
+            self.excel_file, sheet_name=excel_sheet)
+        judges = self.get_groups(frame1, CreateAsset.JUDGES_COLS)
+        papers = self.get_groups(frame1, CreateAsset.PAPER_COLS)
+
+        # [print(index, judge) for index, judge in judges]
+        # [print(index, p) for index, p in papers]
+
+        # with ExcelWriter(self.outfile) as writer:
+
+        # for index, group in papers:
+        #     pass  # index, group
+
+    def scoresheets(self, excel_sheet, category: str = None):
+        """Make each group's scoresheets from main spreadsheet data."""
+        frame2 = pd.read_excel(
+            self.excel_file, sheet_name=excel_sheet)
+        grouped_papers = self.get_groups(frame2, CreateAsset.PAPER_COLS)
+
+        fns = []
+
+        for index, group in grouped_papers:
+            fn = f"WARC {self.award} Scoresheet - {category} - GROUP {index}.csv" \
+                if category else f"WARC {self.award.upper()} Scoresheet - GROUP {index}.csv"
+
+            group.set_index(['ID', 'Ref'], inplace=True)
+            group.drop(['PaperGroup'], axis=1, inplace=True)
+
+            group.to_csv(Path(self.path) / fn)
+            fns.append(fn)
+
+        return fns
+
+    def final_picks(self, excel_sheet):
+        pass
 
 
 if __name__ == '__main__':
 
-    file = r'T:\Ascential Events\WARC\Public\WARC.com\Editorial\Awards (Warc)\2020 Awards\2. MENA Prize\Returned scoresheets\Aakriti Goel - GROUP 2.xlsx'
+    scoresheet_file = r'T:\Ascential Events\WARC\Public\WARC.com\Editorial\Awards (Warc)\2020 Awards\2. MENA Prize\Returned scoresheets\Aakriti Goel - GROUP 2.xlsx'
+    infile = r'D:\2021 Awards\2021 2. MENA Prize\MENA 2021 EDIT.xlsx'
     outfile = r'C:\Users\arondavidson\Scripts\Test\2. MENA Prize\test.csv'
-    SS = Scoresheet(file)
-    scores = SS.read_scores()
-    judge = SS.get_judge()
-    print(judge, scores.Score.array)
+    path = r'C:\Users\arondavidson\Scripts\Test\2. MENA Prize'
+    DEFAULT_CREATE = 'marks'
+    award = 'mena'
+    sheetnum = 1
+    # SS = Scoresheet(scoresheet_file)
+    # scores = SS.read_scores()
+    # judge = SS.get_judge()
+    # print(judge)
     # CS = Collate(scores)
-    # if
+    create = CreateAsset(
+        path=path,
+        award=award,
+        outfile=outfile,
+        excel_file=infile,
+        create_type=DEFAULT_CREATE,
+    )
+
+    create.consolidated_marks(sheetnum)
+    print(create.scoresheets(sheetnum))
+    # create.final_picks()
