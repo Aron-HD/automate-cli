cfe8dcf9adb772c696889135fcfa3070ad033623
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-05-19 00:25:29 +0100

    renamed Scoresheet class and worked on scoring calculations, made recursive function for finding scores more robust, allowed Nan values for correct calculations and commented throughout

--- automate/service/scoring.py
+++ automate/service/scoring.py
@@ -15,39 +15,40 @@ class Collate:
         return
 
 
-class Scoresheet:
-    """docstring for Scoresheet."""
+class JudgeScores:
+    """Read score data, judge details and calculations based on scores from scoresheet."""
 
     def __init__(self, scoresheet):
         self.scoresheet = Path(scoresheet)
+        self.judge_scores = None
+        self.judge = {}
 
     def read_scores(self):
         """Read the scores from the scoresheet and return a dataframe."""
 
-        df = pd.read_excel(self.scoresheet, na_filter=False,
+        df = pd.read_excel(self.scoresheet,  # na_filter=False,
                            index_col=None, header=None)
-        # get score rows only by seeing if ID integers are present
-        score_rows = df[df[0].str.isdigit()]
-        # paper cols
-        ids, refs, papers = score_rows[0], score_rows[1], score_rows[2]
+        # get score rows only by seeing if ID integers are present after filtered first col NaNs
+        score_rows = df[df[0].fillna('').str.isdigit()]
+
+        # ToDo - sort on ID
 
         def find_scores(col: int):
             # base case
-            if all(score_rows.iloc[:, col].astype(str).str.isnumeric()):
-                return score_rows.iloc[:, col]
-            else:
-                print(f'Scores not found in col: {col}')
+            try:
+                # convert so can hold NA
+                return score_rows.iloc[:, col].astype('Int64')
+            except IndexError:
                 return find_scores(col - 1)
 
         # find total score col recursively
-        totals = find_scores(col=9)
-
+        totals = find_scores(col=10)
+        # concat paper cols with total scores and drop index
         score_data = pd.concat(
-            [ids, refs, papers, totals], axis=1).reset_index(drop=True)
-
+            [score_rows[0], score_rows[1], score_rows[2], totals], axis=1).reset_index(drop=True)
         # rename columns
         score_data.columns = ['ID', 'Ref', 'Paper', 'Score']
-
+        self.judge_scores = score_data
         return score_data
 
     def get_judge(self):
@@ -68,12 +69,30 @@ class Scoresheet:
 
         if judge_info:
             try:
-                # Extract group integer from group string
+                # Extract group integer from group string in filename
                 judge_info['Group'] = int(
                     ''.join(filter(str.isdigit, judge_info['Group'])))
+
+                self.judge.update(judge_info)
             except TypeError as e:
                 raise e
+
         return judge_info
+
+    def calculate_judge_formulas(self):
+
+        if self.judge_scores is not None:
+
+            sc = self.judge_scores.Score
+            judge_formulas = {
+                'ScoreCount': sc.count(),  # check if counts 0 and nan
+                'ScoreAverage': sc.mean(),  # sc.sum()/count
+                'ScoreMinmax': sc.max()-sc.min()
+            }
+
+            return self.judge.update(judge_formulas)
+        else:
+            return None
 
 
 class CreateAsset(object):
@@ -154,26 +173,29 @@ class CreateAsset(object):
 
 if __name__ == '__main__':
 
-    scoresheet_file = r'T:\Ascential Events\WARC\Public\WARC.com\Editorial\Awards (Warc)\2020 Awards\2. MENA Prize\Returned scoresheets\Aakriti Goel - GROUP 2.xlsx'
+    scoresheet_file = r"T:\Ascential Events\WARC\Public\WARC.com\Editorial\Awards (Warc)\2020 Awards\2. MENA Prize\Returned scoresheets\Tarek El Kady - GROUP 3.xlsx"
     infile = r'D:\2021 Awards\2021 2. MENA Prize\MENA 2021 EDIT.xlsx'
     outfile = r'C:\Users\arondavidson\Scripts\Test\2. MENA Prize\test.csv'
     path = r'C:\Users\arondavidson\Scripts\Test\2. MENA Prize'
     DEFAULT_CREATE = 'marks'
     award = 'mena'
     sheetnum = 1
-    # SS = Scoresheet(scoresheet_file)
-    # scores = SS.read_scores()
-    # judge = SS.get_judge()
-    # print(judge)
-    # CS = Collate(scores)
-    create = CreateAsset(
-        path=path,
-        award=award,
-        outfile=outfile,
-        excel_file=infile,
-        create_type=DEFAULT_CREATE,
-    )
+    JS = JudgeScores(scoresheet_file)
+    JS.get_judge()
+    JS.read_scores()
+    JS.calculate_judge_formulas()
+    print(JS.judge_scores)
+    print(JS.judge)
 
-    create.consolidated_marks(sheetnum)
-    print(create.scoresheets(sheetnum))
+    # CS = Collate(scores)
+    # create = CreateAsset(
+    #     path=path,
+    #     award=award,
+    #     outfile=outfile,
+    #     excel_file=infile,
+    #     create_type=DEFAULT_CREATE,
+    # )
+
+    # create.consolidated_marks(sheetnum)
+    # print(create.scoresheets(sheetnum))
     # create.final_picks()
