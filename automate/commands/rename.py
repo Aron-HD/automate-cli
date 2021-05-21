ffbba9bcc8808271c97e2598d1d3763ce87725bb
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-05-21 19:31:11 +0100

    minor changes and formatting

--- automate/commands/rename.py
+++ automate/commands/rename.py
@@ -3,8 +3,8 @@ import pandas as pd
 from automate.service import rename
 
 DEFAULT_INFILE = r"T:\Ascential Events\WARC\Backup Server\Loading\Monthly content for Newgen\Project content - May 2021\2021 Effectiveness Awards\WAFE_2021_EDIT.xlsx"
-DEFAULT_FILESPATH = r'D:\2021 Awards\2021 1. Effectiveness Awards\RENAMETEST'
-DEFAULT_FILE = r'D:\2021 Awards\2021 1. Effectiveness Awards\RENAMETEST\758812_1710344_CaseFilm.mp4'
+DEFAULT_FILESPATH = r'C:\Users\arondavidson\Scripts\Test\_innovation'
+DEFAULT_FILE = r'C:\Users\arondavidson\Scripts\Test\_innovation\758844_1714426_Entrypaper.docx'
 
 
 def get_ids(excelfile, sheet):
@@ -14,26 +14,25 @@ def get_ids(excelfile, sheet):
     click.echo('Select headers:')
     cols = dict(enumerate(df.columns.values))
     [click.echo(f'\t {k} - {v}') for k, v in cols.items()]
-    from_ids = click.prompt(
-        'select col index to rename from', type=int)
-    to_ids = click.prompt(
-        'select col index to rename to', type=int)
+    from_ids = click.prompt('select col index to rename from', type=int)
+    to_ids = click.prompt('select col index to rename to', type=int)
 
     col1 = cols[from_ids]
     col2 = cols[to_ids]
     # has to be int to avoid float numbers
-    ids = dict(zip(df[col1].astype(
-        str).tolist(), df[col2].astype(str).tolist()))
+    ids = dict(
+        zip(df[col1].astype(str).tolist(), df[col2].astype(str).tolist()))
     return ids
 
 
 @click.option(
     "-i",
     "--infile",
-    type=click.Path(
-        exists=True, file_okay=True, dir_okay=False,
-        readable=True, resolve_path=True
-    ),
+    type=click.Path(exists=True,
+                    file_okay=True,
+                    dir_okay=False,
+                    readable=True,
+                    resolve_path=True),
     default=DEFAULT_INFILE,  # remove later
     show_default=True,
     required=True,
@@ -45,15 +44,16 @@ def get_ids(excelfile, sheet):
     default=0,
     required=True,
     show_default=True,
-    help="The sheet within the infile you want to read. This can be the name or an index.",
+    help=
+    "The sheet within the infile you want to read. This can be the name or an index.",
 )
 @click.option(
     "-f",
     "--filepath",
-    type=click.Path(
-        exists=True, file_okay=True, dir_okay=True,
-        resolve_path=True
-    ),
+    type=click.Path(exists=True,
+                    file_okay=True,
+                    dir_okay=True,
+                    resolve_path=True),
     default=DEFAULT_FILESPATH,  # remove later
     show_default=True,
     required=True,
@@ -64,10 +64,12 @@ def get_ids(excelfile, sheet):
     default=True,
     required=True,
     show_default=True,
-    help="The sheet within the infile you want to read. This can be the name or an index.",
+    help=
+    "The sheet within the infile you want to read. This can be the name or an index.",
 )
 @click.option(
-    "-n", "--name-format",
+    "-n",
+    "--name-format",
     type=click.Choice(['v0', '_asset'], case_sensitive=False),
     required=True,
     help="The format you want files renaming with.",
