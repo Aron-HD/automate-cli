21d6bb4ddbe8be4cc434c838d3a196bc87066039
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-05-26 00:05:42 +0100

    minor additions to beginning of test

--- tests/scoring_test.py
+++ tests/scoring_test.py
@@ -1,2 +1,22 @@
 import pytest
 from automate.service import scoring
+
+
+@pytest.fixture
+def wf(example_data):
+    pass
+    # return WF(**example_data[0])
+
+
+@pytest.fixture
+def example_data():
+    return [
+        {
+            "path": "",
+            "ids": {
+                123456: 654321,
+                758844: 136623,
+            },
+            "name_format": "v0",
+        },
+    ]
