93e4241fc21bcb3acc925a602ce8c58864d09867
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-08-10 17:10:24 +0100

    added writing of config.py dict to config.json

--- automate/__init__.py
+++ automate/__init__.py
@@ -1,1 +1,8 @@
+import json
+from config import CUSTOM_INPUTS
 from automate.settings import SETTINGS
+
+
+with open('config.json', 'w') as j:
+    jstr = json.dumps(CUSTOM_INPUTS, indent=4)
+    j.write(jstr)
